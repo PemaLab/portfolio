2c9ef2c09a30bc1a706ae5c9c8f9f21f04cfa8a6
PemaLab <105812727+PemaLab@users.noreply.github.com>
2022-10-03 17:27:12 -0400

    initialcommit

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-let navbar = document.getElementById("navBar");
-function showMenu() {
-    navbar.style.right = "-200px";
-}
-function hideMenu() {
-    navbar.style.right = "-520px"
-}
+/*--------NavBar Menu---------*/
+let menu = document.querySelector(".menu");
+let navMenu = document.querySelector(".nav-menu");
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.
+addEventListener("click", () => {
+    menu.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+/*---------------Reveal Sections--------------*/
 window.addEventListener('scroll', reveal);
 function reveal (){
     let reveals = document.querySelectorAll('.reveal');
@@ -12,7 +20,7 @@ function reveal (){
     for (let i = 0; i < reveals.length; i++){
         let windowheight = window.innerHeight;
         let revealtop = reveals[i].getBoundingClientRect().top;
-        let revealpoint = 250;
+        let revealpoint = 150;
 
         if(revealtop < windowheight - revealpoint){
             reveals[i].classList.add('active');
@@ -22,3 +30,7 @@ function reveal (){
         }
     }
 }
+function toggle(){
+    let popup = document.querySelector(".popup-video")
+    popup.classList.toggle("active")
+}    
