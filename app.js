babd025e1c3b5c2f413a546a01346a29b9ed866a
PemaLab <105812727+PemaLab@users.noreply.github.com>
2024-09-06 18:15:06 -0400

    intialcommit

--- app.js
+++ app.js
@@ -7,26 +7,3 @@ document.querySelector(".toggle").onclick = function () {
 /*---------------Reveal Sections--------------*/
 
 /*-----------------------------Contact-----------------------*/
-
-const btn = document.getElementById('button');
-const formToReset = document.getElementById('contact-form');
-
-document.getElementById('contact-form')
-.addEventListener('submit', function(event) {
-event.preventDefault();
-formToReset.reset();
-
-btn.value = 'Sending...';
-
-const serviceID = 'default_service';
-const templateID = 'template_2dvt0rl';
-
-emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-    btn.value = 'Send Email';
-    alert('Sent!');
-    }, (err) => {
-    btn.value = 'Send Email';
-    alert(JSON.stringify(err));
-    });
-});
